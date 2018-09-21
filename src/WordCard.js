cc87ee65976b406aea3b117fe4827d29ee7afbec
Nutchasan <Do_Nutchasan@hotmail.com>
2018-09-21 14:46:37 +0700

    Constructor for  class WordCard

--- src/WordCard.js
+++ src/WordCard.js
@@ -15,8 +15,10 @@ const prepareStateFromWord = (given_word) => {
 }
 
 export default class WordCard extends Component {
-    
-   
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
 
     activationHandler = (c) => {
         let guess = [...this.state.guess, c]
