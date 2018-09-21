e760f65911f1f0a995904e575bb55d0a86a19c67
Nutchasan <Do_Nutchasan@hotmail.com>
2018-09-21 19:23:04 +0700

    Modify Attempt in CharactorCard

--- src/WordCard.js
+++ src/WordCard.js
@@ -10,7 +10,7 @@ const prepareStateFromWord = (given_word) => {
         chars,
         attempt: 1,
         guess: [],
-        completed: true
+        completed: false
     }
 }
 
@@ -34,7 +34,7 @@ export default class WordCard extends Component {
         if(guess.length == this.state.chars.length){
             if(guess.join('').toString() == this.state.word){
                 console.log("YES!!!")
-                this.setState({guess: [], completed: false})
+                this.setState({guess: [], completed: true})
                 console.log("completed : " + this.state.completed)
             }else{
                 this.setState({guess: [], attempt: this.state.attempt + 1})
@@ -48,7 +48,7 @@ export default class WordCard extends Component {
     render() {
         return (
             <div>
-                { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler} />) }
+                { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler} />) }
             </div>
         );
     }
